6c6547383f8c3dfd635623f9a88e209ecf900414
Stephan Foster <sxfoster@Stephans-MacBook-Pro.local>
2017-08-13 15:29:23 -0700

    Added in child classes

---
+++ WIP/tester.java
@@ -0,0 +1,15 @@
+package application;
+
+public class tester {
+
+	public static void main(String[] args) {
+		printTest(new Candy());
+		printTest(new Chips());
+		printTest(new Gum());
+
+	}
+	public static void printTest(Product prod) {
+		System.out.print(prod.getClass().getName() + ":\n" + prod.toString() + "\n\n");
+	}
+
+}
