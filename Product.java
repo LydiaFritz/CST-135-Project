44054a2d5ea19a43d722bd4dc4984989e5edef6f
LydiaFritz <Lydia.Fritz@gcu.edu>
2017-08-14 09:28:38 -0700

    YOUR PRODUCT FILE WITH INSTRUCTOR COMMENTS ADDED
    please confirm that you can all see this.
    
    thanks!

--- Product.java
+++ Product.java
@@ -1,13 +1,20 @@
+//SEE MY COMMENTS - LYDIA
+
+//MUST INCLUDE NAME AND STATEMENT THAT WORK IS YOUR OWN AT THE TOP OF EVERY FILE
+
 package application;
 
+//ABSTRACT SHOULD BE INDICATED IN UML DIAGRAM
 public abstract class Product {
 
+	//NEED TO ADD A FIELD FOR THE QUANTITY
 	String name;
 	double price;
 	
 	Product() {
 	}
 	
+	//WHY IS THERE AN ARGUMENT FOR TYPE?  YOU AREN'T USING IT HERE.
 	Product(String name, String type, double price) {
 		this.name = name;
 		this.price = price;
@@ -19,7 +26,7 @@ public abstract class Product {
 		this.price = prod.price;
 	}
 
-	
+	//THESE SHOULD BE IMPLEMENTED HERE - THEY BEHAVE EXACTLY THE SAME FOR ALL SUBCLASSES
 	//getters and setters
 	abstract String getName();
 	
@@ -35,7 +42,8 @@ public abstract class Product {
     	return "Name: " + this.getName() + "\nPrice: " + this.getPrice();
     }
 
-	
+	//NOT HOW SELLONE WORKS - IF THE PRODUCT HAS A QUANTITY > 0 THEN REDUCE QUANTITY BY 1 AND RETURN TRUE
+	//OTHERWISE RETURN FALSE.
 	//boolean
 	private boolean sellOne(double price) {
 		while (this.price != price)
