fb62060fbdf7eb91471049db38d3e032a45b45a2
Stephan Foster <sxfoster@Stephans-MacBook-Pro.local>
2017-08-13 22:28:02 -0700

    Updated Dispenser class via Zoom meeting

--- WIP/Dispenser.java
+++ WIP/Dispenser.java
@@ -5,32 +5,35 @@ import java.util.List;
 
 public class Dispenser {
 	
-	private ArrayList<Product> products;
-
+	private ArrayList<Product> products = new ArrayList<Product>();
 	Product[] items;
 	int numItems = 0;
 	final int CAPACITY = 100;
 	
 	Dispenser() {
+		products.add(new Candy("Kitkat", 1.75, 1, .6, 1.00, 1.00, "Hersey's", "chocolate bar"));
+		products.add(new Chips("Wavy Lay's", .75, 1, 1.23, .98, 1.45, "Lay's", "Sour Cream and Onion"));
+		products.add(new Drink("Coke", 1.75, 1, "Coca-cola", "Classic"));
+		products.add(new Gum("Juicy Fruit", .89, 1, .55, .75, .98, "Wriggly's", "fruit", 16));
 	}
 
 	//ADD PRODUCT
 	boolean addProduct(Product prod) {
-		for(int i = 0; i < items.length; i++) {
-			((List<Product>) prod).add(prod);
-    	}
-		return false;
-		
+		if(products.size() < CAPACITY) {
+			products.add(prod);
+			return true;			
+		}
+		return false;	
 	}
 	
 	//REMOVE PRODUCT
 	boolean removeProduct(Product prod) {
-		for(int i = 0; i < items.length; i++) {
-			((List<Product>) prod).remove(prod);
-    	}
-		return false;
-
-	}
+		if(products.size() > CAPACITY) {
+			products.remove(prod);
+			return true;
+		}
+			return false;
+		}
 	
 	//CHANGE PRICE 
 	public void changePrice(Product prod, double price) {
@@ -45,12 +48,12 @@ public class Dispenser {
 		return;
 	}
 
+
 	//ITEMS IN AN ARRAY
-	public void displayProducts(Product prod) {
-    	for(int i = 0; i < items.length; i++) {
-    		((List<Product>) prod).get(i).toString();
+	public void displayProducts(){
+    	for(int i = 0; i < products.size(); i++) {
+    		System.out.println(products.get(i).toString());
     	}
-    	return;
 	}
 	
 	//FIND PRODUCTS
@@ -61,7 +64,7 @@ public class Dispenser {
 	
 	//toString()
     public String toString(){
-    	return "Name: " + products.getClass().getName() + ":\n" + products.toString() + "\n\n";
+    	return "Number of Items: " + this.numItems + "\nTotal Capacity: " + this.CAPACITY;
     	
     }
 
--- WIP/tester.java
+++ WIP/tester.java
@@ -3,11 +3,14 @@ package application;
 public class tester {
 
 	public static void main(String[] args) {
-		printTest(new Candy());
-		printTest(new Chips());
-		printTest(new Gum());
-		printTest(new Drink());
+		//printTest(new Candy());
+		//printTest(new Chips());
+		//printTest();
+		//printTest(new Gum());
 
+		Dispenser testDispenser = new Dispenser();
+		testDispenser.displayProducts();
+		
 	}
 	public static void printTest(Product prod) {
 		System.out.print(prod.getClass().getName() + ":\n" + prod.toString() + "\n\n");
